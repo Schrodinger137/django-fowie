add445a98e7bf572768610acbeab7fe7cd3a2daf
Schrodinger137 <elingrato7@gmail.com>
2025-06-24 19:43:52 -0600

    'si'

--- prueba/inicio/views.py
+++ prueba/inicio/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
 # Create your views here.
 
 def principal(request):
+    alumnos = 0
     return render(request, "inicio/principal.html")
 
 def contacto(request):
