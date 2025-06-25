c5f69a9fb7681c53cb66b38eef4c2c598329814a
Schrodinger137 <elingrato7@gmail.com>
2025-06-24 19:15:58 -0600

    agregando fowie-project

---
+++ prueba/registros/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Alumnos(models.Model):
+    matricula = models.CharField(max_length=12, verbose_name='Mat')
+    nombre = models.TextField()
+    carrera = models.TextField()
+    turno = models.CharField(max_length=10)
+    imagen = models.ImageField(null=True, upload_to='fotos', verbose_name='Fotogragía')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Alumno"
+        verbose_name_plural= "Alumnos"
+        ordering = ["-created"]
+        
+    def __str__(self):
+        return self.nombre
