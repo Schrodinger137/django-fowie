ae184c258bb882efc750cd924133bab8fb3ab28b
Schrodinger137 <elingrato7@gmail.com>
2025-08-06 09:35:31 -0600

    siseyor2

--- prueba/prueba/settings.py
+++ prueba/prueba/settings.py
@@ -79,15 +79,10 @@ WSGI_APPLICATION = 'prueba.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
-        'USER' : 'root',
-        'PASSWORD': 'fowie',
-        'PORT': '3306',
-        'NAME': 'ejemplo',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
